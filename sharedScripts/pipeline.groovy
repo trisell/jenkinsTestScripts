059ddcac94a0d5fcabc79944a8b8f4c3584c498b
ddesimini@gmail.com <domdesemini@Blues-MacBook-Pro.local>
2019-08-26 09:33:32 -0600

    adding kube config

--- sharedScripts/pipeline.groovy
+++ sharedScripts/pipeline.groovy
@@ -1,6 +1,29 @@
 #!groovy
 pipeline {
-    agent linux 
+    agent  {
+    	kubernetes{
+      label 'jenkins-slave'
+      defaultContainer 'jnlp'
+      yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    name: init
+spec:
+  containers:
+  - name: base
+    image: jenkins/slave:alpine 
+    command:
+    - cat
+    tty: true
+    resources:
+      limits:
+      memory: 200Mi
+
+"""
+	}
+    }
         stages {
             stage('Stage 1') {
                 steps {
